91c0ba7ec1b33dbcab76c94c93dce635bd50523c
Mokych Andrey <mokych.andrey@gmail.com>
2019-06-12 15:28:32 +0300

    Added button to change default fee asset into transaction confirm

--- app/components/Blockchain/Transaction.jsx
+++ app/components/Blockchain/Transaction.jsx
@@ -22,6 +22,17 @@ import ReactTooltip from "react-tooltip";
 import moment from "moment";
 import {Link, DirectLink} from "react-scroll";
 import {Tooltip} from "bitshares-ui-style-guide";
+import SetDefaultFeeAssetModal from "../Modal/SetDefaultFeeAssetModal";
+import {
+    Link,
+    DirectLink,
+    Element,
+    Events,
+    animateScroll as scroll,
+    scrollSpy,
+    scroller
+} from "react-scroll";
+import {Tooltip, Button} from "bitshares-ui-style-guide";
 import asset_utils from "../../lib/common/asset_utils";
 import sanitize from "sanitize";
 
@@ -118,6 +129,11 @@ class OperationTable extends React.Component {
 }
 
 class Transaction extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
     componentDidMount() {
         ReactTooltip.rebuild();
     }
@@ -2374,7 +2390,35 @@ class Transaction extends React.Component {
             );
         });
 
-        return <div>{info}</div>;
+        return (
+            <div>
+                {info}
+                <Button
+                    key="open_change_fee_asset"
+                    type="secondary"
+                    style={{float: "right"}}
+                    onClick={() => {
+                        this.setState({showModal: true});
+                    }}
+                >
+                    {counterpart.translate("settings.change_default_fee_asset")}
+                </Button>
+                <SetDefaultFeeAssetModal
+                    key="change_fee_asset_modal"
+                    className="modal"
+                    show={this.state.showModal}
+                    current_asset={this.state.current_asset}
+                    displayFees={false}
+                    forceDefault={true}
+                    onChange={value => {
+                        this.setState({current_asset: value});
+                    }}
+                    close={() => {
+                        this.setState({showModal: false});
+                    }}
+                />
+            </div>
+        );
     }
 }
 
