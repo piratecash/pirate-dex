54a4b9584344f298e33ddeda37833b7ebace0654
Stefan Schießl <stefan.schiessl@blockchainprojectsbv.com>
2019-06-22 21:27:59 +0200

    fixed formatting and re added translation
    Signed-off-by: Stefan Schießl <stefan.schiessl@blockchainprojectsbv.com>

--- app/components/Explorer/Witnesses.jsx
+++ app/components/Explorer/Witnesses.jsx
@@ -298,8 +298,8 @@ class WitnessList extends React.Component {
                         ? -1
                         : a.lastConfirmedBlock.timestamp <
                           b.lastConfirmedBlock.timestamp
-                        ? 1
-                        : 0;
+                            ? 1
+                            : 0;
                 }
             },
             {
@@ -322,8 +322,8 @@ class WitnessList extends React.Component {
                     return a.blocksMissed > b.blocksMissed
                         ? 1
                         : a.blocksMissed < b.blocksMissed
-                        ? -1
-                        : 0;
+                            ? -1
+                            : 0;
                 }
             },
             {
@@ -507,6 +507,9 @@ class Witnesses extends React.Component {
                             </div>
 
                             <SearchInput
+                                placeholder={counterpart.translate(
+                                    "explorer.witnesses.filter_by_name"
+                                )}
                                 value={this.state.filterWitness}
                                 onChange={this._onFilter.bind(this)}
                                 style={{
