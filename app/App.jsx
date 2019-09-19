14a152416de3c4f887dca3c1015d6117079c327b
Stefan Schiessl <stefan.schiessl@blockchainprojectsbv.com>
2019-09-19 14:01:04 +0200

    introduce quicktrade router
    Signed-off-by: Stefan Schiessl <stefan.schiessl@blockchainprojectsbv.com>

--- app/App.jsx
+++ app/App.jsx
@@ -182,7 +182,7 @@ const DirectDebit = Loadable({
 
 const QuickTrade = Loadable({
     loader: () =>
-        import(/* webpackChunkName: "QuickTrade" */ "./components/QuickTrade/QuickTrade"),
+        import(/* webpackChunkName: "QuickTrade" */ "./components/QuickTrade/QuickTradeRouter"),
     loading: LoadingIndicator
 });
 
@@ -572,12 +572,7 @@ class App extends React.Component {
                                 />
                                 <Route
                                     exact
-                                    path="/quick-trade/:sell"
-                                    component={QuickTrade}
-                                />
-                                <Route
-                                    exact
-                                    path="/quick-trade/:sell/:receive"
+                                    path="/quick-trade/:marketID"
                                     component={QuickTrade}
                                 />
                                 <Route path="*" component={Page404} />
