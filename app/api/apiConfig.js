b62afdadf29a803b915f885bd8c0f8b4309f1d37
Abit <abitmore@users.noreply.github.com>
2021-09-07 19:11:51 +0200

    Remove clockwork's API node

--- app/api/apiConfig.js
+++ app/api/apiConfig.js
@@ -397,14 +397,6 @@ export const settingsAPIs = {
             contact: "telegram:gordoor;wechat:overyard"
         },
         {
-            url: "wss://bts-seoul.clockwork.gr",
-            region: "Southeastern Asia",
-            country: "Korea",
-            location: "Seoul",
-            operator: "Witness: clockwork",
-            contact: "telegram:clockworkgr"
-        },
-        {
             url: "wss://api.61bts.com",
             region: "Eastern Asia",
             country: "China",
