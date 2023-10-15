0516835135d9f238acc0eb74102ce575a81d82e8
abitmore <abitmore@users.noreply.github.com>
2023-10-15 14:10:57 -0400

    Add a phishing account

--- app/lib/common/scamAccounts.js
+++ app/lib/common/scamAccounts.js
@@ -1807,6 +1807,8 @@ export const scamAccountsOther = [
     "1.2.1800498",
     "bihance-bts-1",
     "1.2.1800807",
+    "binacne-bts-0103",
+    "1.2.1828534",
     "binaence-bts-1",
     "1.2.1795582",
     "binaince-bts-1",
